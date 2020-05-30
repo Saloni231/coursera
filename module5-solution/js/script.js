baec44553d1584a45ad266e78864bfaf278e586e
SALONI <ghagsaloni231@gmail.com>
2020-05-30 14:03:18 +0530

    done

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -102,7 +102,6 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       var chosenCategoryShortName = chooseRandomCategory(categories);
-      console.log(chosenCategoryShortName);
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -118,7 +117,6 @@ function buildAndShowHomeHTML (categories) {
       //
       var homeHtmlToInsertIntoMainPage = homeHtml;
       var str = "'" + chosenCategoryShortName.short_name + "'";
-      console.log(str);
       homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlToInsertIntoMainPage,"randomCategoryShortName",str);
 
 
